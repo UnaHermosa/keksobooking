e507ae15dc6ce4d5b175cedbc27c09b5ed8f853e
Елена Кравчишина <veintitress@yandex.ru>
2022-10-12 20:46:58 +0300

    Timein/timeout selects bounded

--- js/form.js
+++ js/form.js
@@ -1,6 +1,33 @@
-const typeOfHousing = document.querySelector('#type');
+const timeInSelect = document.querySelector('#timein');
+const timeOutSelect = document.querySelector('#timeout');
 
+const onTimeInChange = () => {
+  switch(timeInSelect.value) {
+    case '12:00':
+      timeOutSelect.value = '12:00';
+      break;
+    case '13:00':
+      timeOutSelect.value = '13:00';
+      break;
+    case '14:00':
+      timeOutSelect.value = '14:00';
+      break;
+  }
+};
 
-console.log(typeOfHousing);
+const onTimeOutChange = () => {
+  switch(timeOutSelect.value) {
+    case '12:00':
+      timeInSelect.value = '12:00';
+      break;
+    case '13:00':
+      timeInSelect.value = '13:00';
+      break;
+    case '14:00':
+      timeInSelect.value = '14:00';
+      break;
+  }
+};
 
-
+timeInSelect.addEventListener('change', onTimeInChange);
+timeOutSelect.addEventListener('change', onTimeOutChange);
