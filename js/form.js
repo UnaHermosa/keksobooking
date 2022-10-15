e700743cc03ff2faa694c9b662e63f3eb94e5221
Елена Кравчишина <veintitress@yandex.ru>
2022-10-15 09:31:45 +0300

    Type of housing filtering done

--- js/form.js
+++ js/form.js
@@ -1,24 +1,46 @@
-const typeOfHousing = document.querySelector('[name = type]');
+export { onTimeSelectChange, onTypeOfHousingSelectChange };
 
-const onTimeSelectChange = (value1, value2) => {
+const onTimeSelectChange = (firstElement, secondElement) => {
   return (evt) => {
     evt.preventDefault();
-    switch(value1.value) {
+    switch(firstElement.value) {
       case '12:00':
-        value2.value = '12:00';
+        secondElement.value = '12:00';
         break;
       case '13:00':
-        value2.value = '13:00';
+        secondElement.value = '13:00';
         break;
       case '14:00':
-        value2.value = '14:00';
+        secondElement.value = '14:00';
         break;
     }
   };
 };
 
-const onTypeOfHousingSelectChange = () => {
-  
+const onTypeOfHousingSelectChange = (firstElement, secondElement) => {
+  return (evt) => {
+    evt.preventDefault();
+    switch (firstElement.value) {
+      case 'bungalow':
+        secondElement.placeholder = '0';
+        secondElement.min = '0';
+        break;
+      case 'flat':
+        secondElement.placeholder = '1000';
+        secondElement.min = '1000';
+        break;
+      case 'hotel':
+        secondElement.placeholder = '3000';
+        secondElement.min = '3000';
+        break;
+      case 'house':
+        secondElement.placeholder = '5000';
+        secondElement.min = '5000';
+        break;
+      case 'palace':
+        secondElement.placeholder = '10000';
+        secondElement.min = '10000';
+        break;
+    }
+  };
 };
-
-export { onTimeSelectChange };
--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 import { createAdvertismentsArray } from './data.js';
 import './popup.js';
-import { onTimeSelectChange } from './form.js'
+import { onTimeSelectChange, onTypeOfHousingSelectChange } from './form.js'
 
 const timeInSelect = document.querySelector('#timein');
 const timeOutSelect = document.querySelector('#timeout');
+const typeOfHousing = document.querySelector('[name = type]');
+const priceInput = document.querySelector('#price');
 
 createAdvertismentsArray();
 
 timeInSelect.addEventListener('change', onTimeSelectChange(timeInSelect, timeOutSelect));
 timeOutSelect.addEventListener('change', onTimeSelectChange(timeOutSelect, timeInSelect));
+typeOfHousing.addEventListener('change', onTypeOfHousingSelectChange(typeOfHousing, priceInput));
